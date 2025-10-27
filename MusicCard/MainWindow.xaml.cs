d6cf861fb5a1bc101dbef038dfbd31e5364fd7f1
mikita12 <patronikm@gmail.com>
2025-10-27 21:50:13 +0100

    up

--- MusicCard/MainWindow.xaml.cs
+++ MusicCard/MainWindow.xaml.cs
@@ -1,33 +1,85 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.InteropServices;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.Storage;
+using WinRT; // for InitializeWithWindow
 
 namespace MusicCard
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private string? _filePath;
+
+        [Flags]
+        private enum PlaySoundFlags : int
+        {
+            SND_SYNC = 0x0000,
+            SND_ASYNC = 0x0001,
+            SND_NODEFAULT = 0x0002,
+            SND_MEMORY = 0x0004,
+            SND_LOOP = 0x0008,
+            SND_NOSTOP = 0x0010,
+            SND_NOWAIT = 0x00002000,
+            SND_ALIAS = 0x00010000,
+            SND_ALIAS_ID = 0x00110000,
+            SND_FILENAME = 0x00020000,
+            SND_RESOURCE = 0x00040004
+        }
+
+        [DllImport("winmm.dll", CharSet = CharSet.Auto)]
+        private static extern bool PlaySound(string? pszSound, IntPtr hmod, PlaySoundFlags fdwSound);
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
 
-            
+            // Pod³¹czamy obs³ugê zdarzeñ do istniej¹cych kontrolek z XAML
+            SelectFileButton.Click += ChooseButton_Click;
+
+            // Dla demonstracji: przypiszemy Start/Stop PlaySound do sekcji "PlaySound" (StartOneButton / StopOneButton)
+            StartOneButton.Click += PlayStartButton_Click;
+            StopOneButton.Click += PlayStopButton_Click;
+
+            // Na start przyciski wy³¹czone dopóki nie wybierzemy pliku
+            StartOneButton.IsEnabled = false;
+            StopOneButton.IsEnabled = false;
+            // Opcjonalnie wy³¹cz Pause jeœli nieobs³ugiwany
+            PauseOneButton.IsEnabled = false;
+        }
+
+        private async void ChooseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.FileTypeFilter.Add(".wav");
+
+            // WinUI3: trzeba zainicjalizowaæ picker oknem natywnym
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+
+            StorageFile? file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                _filePath = file.Path;
+                SelectedFileText.Text = _filePath;
+                StartOneButton.IsEnabled = true;
+                StopOneButton.IsEnabled = true;
+            }
+        }
+
+        private void PlayStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(_filePath))
+                return;
+
+            // Asynchroniczne odtwarzanie pliku
+            PlaySound(_filePath, IntPtr.Zero, PlaySoundFlags.SND_FILENAME | PlaySoundFlags.SND_ASYNC);
+        }
+
+        private void PlayStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Zatrzymuje bie¿¹ce PlaySound
+            PlaySound(null, IntPtr.Zero, 0);
         }
     }
 }
