5f5a6175707278a0b9e6fc8ad046e16039a88929
mrater <maciejosikorski@gmail.com>
2025-10-27 21:46:19 +0100

    addss

--- MusicCard/MainWindow.xaml.cs
+++ MusicCard/MainWindow.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +5,17 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,11 +27,41 @@ namespace MusicCard
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        [DllImport("winmm.dll", SetLastError = true)]
+        private static extern bool PlaySound(string pszSound, IntPtr hmod, uint fdwSound);
+        public StorageFile? SelectedSound { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            
+
         }
+        private async void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".wav");
+
+            // Trzeba po³¹czyæ picker z oknem WinUI
+            var hwnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(picker, hwnd);
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                SelectedSound = file;
+                SelectedFileText.Text = $"Selected: {file.Name}";
+            }
+            else
+            {
+                SelectedFileText.Text = "No file selected...";
+            }
+        }
+
+        public void playMusic()
+        {
+            PlaySound("music.wav", IntPtr.Zero, 0x0001);
+        }
+
     }
 }
